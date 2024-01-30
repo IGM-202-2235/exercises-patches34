927b3fabd06445c61e179aa456eb61e3be7bd2d3
eabigm <eabigm@MSS2150-00>
2024-01-30 12:17:50 -0500

    week 3day 1

---
+++ Exercise 3/Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    float rotationRate = 360f / 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotationRate);
+    }
+}
