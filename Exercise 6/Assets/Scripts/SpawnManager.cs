03c2c228922cd0723103367f6d1fa5aa10c9fbde
eabigm <eabigm@MSS2150-00>
2024-02-22 12:17:00 -0500

    week 6 day 2

---
+++ Exercise 6/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimalTypes
+{
+    Elephant,
+    Turtle,
+    Snail,
+    Octopus,
+    Kanagroo
+}
+
+public class SpawnManager : Singleton<SpawnManager>
+{
+    // (Optional) Prevent non-singleton constructor use.
+    protected SpawnManager() { }
+
+    public SpriteRenderer animalPrefab;
+
+    public List<Sprite> animalSprites;
+
+    public int animalCount = 100;
+
+    public List<SpriteRenderer> spawnedAnimals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Spawn()
+    {
+        CleanUp();
+        
+        for(int i = 0; i < animalCount; i++)
+        {
+            SpawnAnimal();
+        }
+    }
+
+    public void SpawnAnimal()
+    {
+        SpriteRenderer newAnimal = Instantiate(animalPrefab);
+
+        //  Change sprite
+        newAnimal.sprite = animalSprites[(int)PickRandomAnimal()];
+
+        //  Change color
+        newAnimal.color = Random.ColorHSV(0, 1, 1, 1, 1, 1);
+
+        //  Set position
+        float screenH = Camera.main.orthographicSize * 2f;
+        float screenW = screenH * Camera.main.aspect;
+
+        float x = Gaussian(0, screenW / 8f);
+        float y = Gaussian(0, screenH / 8f);
+
+        newAnimal.transform.position = new Vector3(x, y, 0);
+
+        spawnedAnimals.Add(newAnimal);
+    }
+
+    void CleanUp()
+    {
+        foreach(SpriteRenderer animal in spawnedAnimals)
+        {
+            Destroy(animal.gameObject);
+        }
+
+        spawnedAnimals.Clear();
+    }
+
+
+    float Gaussian(float mean, float stdDev)
+    {
+        float val1 = Random.Range(0f, 1f);
+        float val2 = Random.Range(0f, 1f);
+
+        float gaussValue =
+                 Mathf.Sqrt(-2.0f * Mathf.Log(val1)) *
+                 Mathf.Sin(2.0f * Mathf.PI * val2);
+
+        return mean + stdDev * gaussValue;
+    }
+
+    AnimalTypes PickRandomAnimal()
+    {
+        float randValue = Random.Range(0f, 1f);
+
+        if(randValue < .6f)
+        {
+            return AnimalTypes.Elephant;
+        }
+        else if(randValue < .84f)
+        {
+            return AnimalTypes.Snail;
+        }
+        else
+        {
+            return AnimalTypes.Octopus;
+        }
+    }
+
+}
